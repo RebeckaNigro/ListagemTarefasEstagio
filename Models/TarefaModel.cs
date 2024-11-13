5e6d87a6fdaa3eeb9149a899da7e90884e57d2bb
RMDomingos20 <112645680+RMDomingos20@users.noreply.github.com>
2024-11-13 11:29:54 -0300

    Update TarefaModel.cs

--- Models/TarefaModel.cs
+++ Models/TarefaModel.cs
@@ -1,15 +1,12 @@
-﻿using ListagemTarefasEstagio.Enums;
-
 namespace ListagemTarefasEstagio.Models
 {
     public class TarefaModel
     {
-        public int Id { get; set; }
-        public string? Titulo { get; set; }
-        public string? Descrição { get; set; }
-        public StatusDaTarefa Status { get; set; }
-        public int? UsuarioId { get; set; }
-        public virtual UsuarioModel? Usuario { get; set; }
-                
+        public int Id { get; set; } // Identificador único da tarefa
+        public string Nome { get; set; } // Nome ou título da tarefa
+        public string Descricao { get; set; } // Descrição detalhada da tarefa
+        public bool Concluido { get; set; } // Indica se a tarefa está concluída ou não
+        public int UsuarioId { get; set; } // Identificador do usuário associado à tarefa
+        public UsuarioModel Usuario { get; set; } // Referência ao usuário associado à tarefa
     }
 }
